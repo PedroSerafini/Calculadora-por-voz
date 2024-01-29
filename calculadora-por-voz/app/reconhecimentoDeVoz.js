44d01a4821cead7d7e53438e6d192c79c6205664
Pedro Henrique Serafini <111706609+PedroSerafini@users.noreply.github.com>
2024-01-29 15:18:25 -0300

    First commit
    Primeiro commit. 
    HTML e CSS prontos.
    Iniciando Script.

---
+++ calculadora-por-voz/app/reconhecimentoDeVoz.js
@@ -0,0 +1,21 @@
+const elementoNumero = document.querySelector('.display');
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'pt-br'
+recognition.start()
+
+recognition.addeventListener('result', onSpeak)
+
+function onSpeak(e) {
+    numero = e.results[0][0].transcript
+    exibeNumero(numero)
+}
+
+function exibeNumero(numero) {
+    elementoNumero
+}
+
+recognition.addeventListener('end', () => recognition.start())
